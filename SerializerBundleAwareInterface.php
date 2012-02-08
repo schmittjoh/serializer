83dce155012f3e427c212e54f952b26b728b03c8
Oleg Zinchenko <olegz@default-value.com>
2012-02-08 18:09:16 +0200

    added missed use statement

--- SerializerBundleAwareInterface.php
+++ SerializerBundleAwareInterface.php
@@ -2,6 +2,8 @@
 
 namespace JMS\SerializerBundle;
 
+use JMS\SerializerBundle\DependencyInjection\JMSSerializerExtension;
+
 interface SerializerBundleAwareInterface
 {
     function configureSerializerExtension(JMSSerializerExtension $extension);
