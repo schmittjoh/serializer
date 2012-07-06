c009fb51e3234823077e3cb5f2656fd2916d1154
Roger Llopart Pla <lumbendil@gmail.com>
2012-07-06 11:46:24 +0200

    Added php metadata for the ObjectWithVirtualPropertiesAndExcludeAll fixture.

---
+++ Tests/Metadata/Driver/php/ObjectWithVirtualPropertiesAndExcludeAll.php
@@ -0,0 +1,15 @@
+<?php
+
+use JMS\SerializerBundle\Metadata\ClassMetadata;
+use JMS\SerializerBundle\Metadata\PropertyMetadata;
+use JMS\SerializerBundle\Metadata\VirtualPropertyMetadata;
+
+$className = 'JMS\SerializerBundle\Tests\Fixtures\ObjectWithVirtualPropertiesAndExcludeAll';
+
+$metadata = new ClassMetadata( $className );
+
+$pMetadata = new VirtualPropertyMetadata($className, 'virtualValue');
+$pMetadata->getter = 'getVirtualValue';
+$metadata->addPropertyMetadata($pMetadata);
+
+return $metadata;
