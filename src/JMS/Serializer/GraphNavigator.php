a526e5940be7b2a1543376fe69b270acab032426
Adrien Brault <adrien.brault@gmail.com>
2012-12-06 10:13:48 +0100

    Remove unused variable

--- src/JMS/Serializer/GraphNavigator.php
+++ src/JMS/Serializer/GraphNavigator.php
@@ -48,7 +48,6 @@ final class GraphNavigator
     private $handlerRegistry;
     private $objectConstructor;
     private $exclusionStrategy;
-    private $customHandlers = array();
     private $visiting;
 
     /**
