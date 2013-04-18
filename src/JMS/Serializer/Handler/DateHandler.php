76f16216d511f93eadda1aa3a5d764e71632a482
Tyler Sommer <sommertm@gmail.com>
2013-04-18 16:12:54 -0700

    Removed static keyword from SubscribingHandlerInterface

--- src/JMS/Serializer/Handler/DateHandler.php
+++ src/JMS/Serializer/Handler/DateHandler.php
@@ -2,13 +2,13 @@
 
 /*
  * Copyright 2013 Johannes M. Schmitt <schmittjoh@gmail.com>
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -30,7 +30,7 @@ class DateHandler implements SubscribingHandlerInterface
     private $defaultFormat;
     private $defaultTimezone;
 
-    public static function getSubscribingMethods()
+    public function getSubscribingMethods()
     {
         $methods = array();
         $types = array('DateTime', 'DateInterval');
