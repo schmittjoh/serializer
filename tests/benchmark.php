7c90f2d98c6a5cdc585a639f8880eb03a2849df9
Johannes M. Schmitt <schmittjoh@gmail.com>
2013-05-15 08:33:15 +0200

    some improvements to benchmark

--- tests/benchmark.php
+++ tests/benchmark.php
@@ -1,5 +1,12 @@
 <?php
 
+if ( ! isset($_SERVER['argv'][1], $_SERVER['argv'][2])) {
+    echo 'Usage: php benchmark.php <format> <iterations> [output-file]'.PHP_EOL;
+    exit(1);
+}
+
+list(, $format, $iterations) = $_SERVER['argv'];
+
 require_once 'bootstrap.php';
 
 function benchmark(\Closure $f, $times = 10) {
@@ -29,14 +36,27 @@ function createObject() {
     return $post;
 }
 
+
+
 $serializer = \JMS\Serializer\SerializerBuilder::create()->build();
 $collection = createCollection();
 $metrics = array();
+$f = function() use ($serializer, $collection, $format) {
+    $serializer->serialize($collection, $format);
+};
 
-foreach (array('json', 'yml', 'xml') as $format) {
-    $metrics['benchmark-collection-'.$format] = benchmark(function() use ($serializer, $collection, $format) {
-        $serializer->serialize($collection, $format);
-    }, 10);
+
+// Load all necessary classes into memory.
+benchmark($f, 1);
+
+printf('Benchmarking collection for format "%s".'.PHP_EOL, $format);
+$metrics['benchmark-collection-'.$format] = benchmark($f, $iterations);
+
+$output = json_encode(array('metrics' => $metrics));
+
+if (isset($_SERVER['argv'][3])) {
+    file_put_contents($_SERVER['argv'][3], $output);
+    echo "Done.".PHP_EOL;
+} else {
+    echo $output.PHP_EOL;
 }
-
-echo json_encode(array('metrics' => $metrics));
