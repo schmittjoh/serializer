6af3b397be261e28e1fa936924a3ebe9883b1bb6
Lukas Kahwe Smith <smith@pooteeweet.org>
2013-11-24 21:51:38 +0100

    added support for PHPCR

--- src/JMS/Serializer/Metadata/Driver/DoctrinePHPCRTypeDriver.php
+++ src/JMS/Serializer/Metadata/Driver/DoctrinePHPCRTypeDriver.php
@@ -19,7 +19,6 @@
 namespace JMS\Serializer\Metadata\Driver;
 
 use Doctrine\Common\Persistence\ManagerRegistry;
-use JMS\Serializer\Metadata\ClassMetadata;
 use Doctrine\Common\Persistence\Mapping\ClassMetadata as DoctrineClassMetadata;
 use JMS\Serializer\Metadata\PropertyMetadata;
 
@@ -27,36 +26,37 @@ use JMS\Serializer\Metadata\PropertyMetadata;
  * This class decorates any other driver. If the inner driver does not provide a
  * a property type, the decorator will guess based on Doctrine 2 metadata.
  */
-class DoctrineTypeDriver extends AbstractDoctrineTypeDriver
+class DoctrinePHPCRTypeDriver extends AbstractDoctrineTypeDriver
 {
-    protected function setDiscriminator(DoctrineClassMetadata $doctrineMetadata, ClassMetadata $classMetadata)
+    /**
+     * @param DoctrineClassMetadata $doctrineMetadata
+     * @param PropertyMetadata $propertyMetadata
+     */
+    protected function hideProperty(DoctrineClassMetadata $doctrineMetadata, PropertyMetadata $propertyMetadata)
     {
-        if (empty($classMetadata->discriminatorMap) && ! $classMetadata->discriminatorDisabled
-            && ! empty($doctrineMetadata->discriminatorMap) && $doctrineMetadata->isRootEntity()
-        ) {
-            $classMetadata->setDiscriminator(
-                $doctrineMetadata->discriminatorColumn['name'],
-                $doctrineMetadata->discriminatorMap
-            );
-        }
+        return 'lazyPropertiesDefaults' === $propertyMetadata->name
+            || $doctrineMetadata->parentMapping === $propertyMetadata->name
+            || $doctrineMetadata->node === $propertyMetadata->name;
     }
 
     protected function setPropertyType(DoctrineClassMetadata $doctrineMetadata, PropertyMetadata $propertyMetadata)
     {
         $propertyName = $propertyMetadata->name;
         if ($doctrineMetadata->hasField($propertyName) && $fieldType = $this->normalizeFieldType($doctrineMetadata->getTypeOfField($propertyName))) {
+            $field = $doctrineMetadata->getField($propertyName);
+            if (!empty($field['multivalue'])) {
+                $fieldType = 'array';
+            }
+
             $propertyMetadata->setType($fieldType);
         } elseif ($doctrineMetadata->hasAssociation($propertyName)) {
-            $targetEntity = $doctrineMetadata->getAssociationTargetClass($propertyName);
-
-            if (null === $targetMetadata = $this->tryLoadingDoctrineMetadata($targetEntity)) {
+            try {
+                $targetEntity = $doctrineMetadata->getAssociationTargetClass($propertyName);
+            } catch (\Exception $e) {
                 return;
             }
 
-            // For inheritance schemes, we cannot add any type as we would only add the super-type of the hierarchy.
-            // On serialization, this would lead to only the supertype being serialized, and properties of subtypes
-            // being ignored.
-            if ($targetMetadata instanceof DoctrineClassMetadata && ! $targetMetadata->isInheritanceTypeNone()) {
+            if (null === $this->tryLoadingDoctrineMetadata($targetEntity)) {
                 return;
             }
 
