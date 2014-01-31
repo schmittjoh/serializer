8d6c61a68b097b2289d6a8d6632a5f4c1715ddce
Chris Donadeo <cdonadeo@voxeo.com>
2014-01-31 01:56:16 -0500

    Fix DoctrineObjectConstructor returning nulls instead of objects
    Makes DoctrineObjectConstructor use the fallback constructor to create
    a new object if the identifier computed from the metadata is not
    currently stored in the database.  This previously just returned a null
    value.

--- src/JMS/Serializer/Construction/DoctrineObjectConstructor.php
+++ src/JMS/Serializer/Construction/DoctrineObjectConstructor.php
@@ -82,8 +82,14 @@ class DoctrineObjectConstructor implements ObjectConstructorInterface
             $identifierList[$name] = $data[$name];
         }
 
-        // Entity update, load it from database
+        // Entity update, try to load it from database
         $object = $objectManager->find($metadata->name, $identifierList);
+        
+        if ( ! is_object($object))
+        {
+        	// Entity with that identifier didn't exist, create a new Entity
+        	return $this->fallbackConstructor->construct($visitor, $metadata, $data, $type, $context);
+        }
 
         $objectManager->initializeObject($object);
 
