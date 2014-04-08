f776b487bbae4e29ac950197fbf26995f2f7cfec
Christian Flothmann <cflothma@uos.de>
2014-04-08 17:34:43 +0200

    fix two dev dependencies that could lead to errors when running the tests

--- tests/JMS/Serializer/Tests/Serializer/BaseSerializationTest.php
+++ tests/JMS/Serializer/Tests/Serializer/BaseSerializationTest.php
@@ -611,7 +611,7 @@ abstract class BaseSerializationTest extends \PHPUnit_Framework_TestCase
 
     public function testConstraintViolation()
     {
-        $violation = new ConstraintViolation('Message of violation', array(), null, 'foo', null);
+        $violation = new ConstraintViolation('Message of violation', 'Message template', array(), null, 'foo', null);
 
         $this->assertEquals($this->getContent('constraint_violation'), $this->serialize($violation));
     }
@@ -619,8 +619,8 @@ abstract class BaseSerializationTest extends \PHPUnit_Framework_TestCase
     public function testConstraintViolationList()
     {
         $violations = new ConstraintViolationList();
-        $violations->add(new ConstraintViolation('Message of violation', array(), null, 'foo', null));
-        $violations->add(new ConstraintViolation('Message of another violation', array(), null, 'bar', null));
+        $violations->add(new ConstraintViolation('Message of violation', 'Message template', array(), null, 'foo', null));
+        $violations->add(new ConstraintViolation('Message of another violation', 'Message template', array(), null, 'bar', null));
 
         $this->assertEquals($this->getContent('constraint_violation_list'), $this->serialize($violations));
     }
