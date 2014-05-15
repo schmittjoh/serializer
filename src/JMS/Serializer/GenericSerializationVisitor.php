4de82ceffcbb2c9db7bdb5eaa96c8e73122f5466
jockri <git@jockri.com>
2014-05-15 17:41:29 +0200

    fixed docblocks

--- src/JMS/Serializer/GenericSerializationVisitor.php
+++ src/JMS/Serializer/GenericSerializationVisitor.php
@@ -157,8 +157,8 @@ abstract class GenericSerializationVisitor extends AbstractVisitor
      * Allows you to add additional data to the current object/root element.
      *
      * @param string $key
-     * @param scalar|array $value This value must either be a regular scalar, or an array.
-     *                            It must not contain any objects anymore.
+     * @param scalar|array|null $value This value must either be a regular scalar, or an array.
+     *                                 It must not contain any objects anymore.
      */
     public function addData($key, $value)
     {
@@ -173,8 +173,8 @@ abstract class GenericSerializationVisitor extends AbstractVisitor
      * Allows you to replace existing data on the current object/root element.
      *
      * @param string $key
-     * @param scalar|array $value This value must either be a regular scalar, or an array.
-     *                            It must not contain any objects anymore.
+     * @param scalar|array|null $value This value must either be a regular scalar, or an array.
+     *                                 It must not contain any objects anymore.
      */
     public function replaceData($key, $value)
     {
