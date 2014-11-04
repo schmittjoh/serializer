a0f24482e3b6e3c2eafa26f7765c7b9f8521a805
Brett Minnie <github@bdb-studios.co.uk>
2014-11-04 14:35:54 +0000

    Initial version of generic serializer annotation

--- src/JMS/Serializer/Metadata/Driver/AnnotationDriver.php
+++ src/JMS/Serializer/Metadata/Driver/AnnotationDriver.php
@@ -219,6 +219,7 @@ class AnnotationDriver implements DriverInterface
                 } else {
                     $propertyMetadata->setAccessor($accessType, $accessor[0], $accessor[1]);
                 }
+
                 if ((ExclusionPolicy::NONE === $exclusionPolicy && !$isExclude)
                     || (ExclusionPolicy::ALL === $exclusionPolicy && $isExpose)) {
                     $classMetadata->addPropertyMetadata($propertyMetadata);
