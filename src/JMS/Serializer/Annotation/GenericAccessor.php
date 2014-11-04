e252bf7faebd0cf7929db419b68760728db52860
Brett Minnie <github@bdb-studios.co.uk>
2014-11-04 14:35:54 +0000

    Initial version of generic serializer annotation

---
+++ src/JMS/Serializer/Annotation/GenericAccessor.php
@@ -0,0 +1,24 @@
+<?php
+
+
+namespace JMS\Serializer\Annotation;
+
+
+final class GenericAccessor
+{
+
+    /**
+     * @var string
+     */
+    public $getter;
+
+    /**
+     * @var string
+     */
+    public $setter;
+
+    /**
+     * @var string
+     */
+    public $propertyName;
+}
