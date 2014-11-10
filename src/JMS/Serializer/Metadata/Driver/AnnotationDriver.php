f12ffb768f3082d396d3fe9408da139cc2f4720f
Brett Minnie <github@bdb-studios.co.uk>
2014-11-10 19:20:21 +0000

    Updated annotation and Drivers to match method, included annotation on GenericAccessor

--- src/JMS/Serializer/Metadata/Driver/AnnotationDriver.php
+++ src/JMS/Serializer/Metadata/Driver/AnnotationDriver.php
@@ -214,8 +214,11 @@ class AnnotationDriver implements DriverInterface
                     }
                 }
 
-                $propertyMetadata->setAccessor($accessType, $accessor[0], $accessor[1], $accessor[2]);
-
+                if (isset($accessor[2])) {
+                    $propertyMetadata->setAccessor($accessType, $accessor[0], $accessor[1], $accessor[2]);
+                } else {
+                    $propertyMetadata->setAccessor($accessType, $accessor[0], $accessor[1]);
+                }
                 if ((ExclusionPolicy::NONE === $exclusionPolicy && !$isExclude)
                     || (ExclusionPolicy::ALL === $exclusionPolicy && $isExpose)) {
                     $classMetadata->addPropertyMetadata($propertyMetadata);
