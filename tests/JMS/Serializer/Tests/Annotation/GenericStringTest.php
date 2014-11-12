feb967aa9809a0b3e177ec61d1f740f457684513
Brett Minnie <github@bdb-studios.co.uk>
2014-11-12 16:02:36 +0000

    Serialization and deserialization with generic accesor tested and working

--- tests/JMS/Serializer/Tests/Annotation/GenericStringTest.php
+++ tests/JMS/Serializer/Tests/Annotation/GenericStringTest.php
@@ -2,7 +2,16 @@
 
 namespace JMS\Serializer\Tests\Annotation;
 
+use Doctrine\Common\Annotations\AnnotationReader;
+use JMS\Serializer\Builder\DefaultDriverFactory;
+use JMS\Serializer\Metadata\Driver\AnnotationDriver;
+use JMS\Serializer\Naming\CamelCaseNamingStrategy;
+use JMS\Serializer\Naming\SerializedNameAnnotationStrategy;
+use JMS\Serializer\Serializer;
+use JMS\Serializer\SerializerBuilder;
 use JMS\Serializer\Tests\Fixtures\GenericStringManipulation;
+use JMS\Serializer\Annotation\Type;
+use JMS\Serializer\Annotation\GenericAccessor;
 
 class testStub
 {
@@ -11,12 +20,34 @@ class testStub
 
 class GenericStringTest extends \PHPUnit_Framework_TestCase
 {
+    protected $factory;
+
+    protected $dispatcher;
+
+    /** @var Serializer */
+    protected $serializer;
+
+    protected $handlerRegistry;
+
+    protected $serializationVisitors;
+
+    protected $deserializationVisitors;
+
     /** @var  testStub */
     protected $stub;
 
     public function setUp()
     {
         $this->stub = new testStub();
+
+        $this->factory = new DefaultDriverFactory(new AnnotationDriver(new AnnotationReader()));
+
+        $namingStrategy = new SerializedNameAnnotationStrategy(new CamelCaseNamingStrategy());
+
+        $this->serializer = SerializerBuilder::create();
+        $this->serializer->setPropertyNamingStrategy($namingStrategy);
+        $this->serializer->setMetadataDriverFactory($this->factory);
+        $this->serializer = $this->serializer->build();
     }
 
     public function testSetUp()
@@ -40,8 +71,8 @@ class GenericStringTest extends \PHPUnit_Framework_TestCase
 
         $stdObject = new \ArrayObject(array('property_one' =>  $expected, 'property_two' => $expected));
 
-        /** @var GenericStringManipulation $testStub */
-        $testStub =  $this->serializer->deserialize(json_encode($stdObject),'JMS\Serializer\Tests\Fixtures\GenericStringManipulation', 'json');
+        /** @var testStub $testStub */
+        $testStub =  $this->serializer->deserialize(json_encode($stdObject),'JMS\Serializer\Tests\Annotation\testStub', 'json');
         $this->assertEquals(strtoupper($expected),json_decode($this->serializer->serialize($testStub, 'json'))->property_one);
         $this->assertEquals(strtoupper($expected),json_decode($this->serializer->serialize($testStub, 'json'))->property_two);
         $this->assertEquals(strtolower($expected),$testStub->getPropertyOne());
