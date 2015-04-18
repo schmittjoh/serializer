06752aa4d5745e53f821e42391be82995f406171
dtonder <delf.tonder@gmx.de>
2015-04-18 22:22:52 +0200

    - added excludeforgroups strategy to setGroups, added metadata-info for annotation

--- src/JMS/Serializer/Metadata/PropertyMetadata.php
+++ src/JMS/Serializer/Metadata/PropertyMetadata.php
@@ -30,6 +30,7 @@ class PropertyMetadata extends BasePropertyMetadata
     public $sinceVersion;
     public $untilVersion;
     public $groups;
+    public $excludeForGroups;
     public $serializedName;
     public $type;
     public $xmlCollection = false;
@@ -104,6 +105,7 @@ class PropertyMetadata extends BasePropertyMetadata
             $this->sinceVersion,
             $this->untilVersion,
             $this->groups,
+            $this->excludeForGroups,
             $this->serializedName,
             $this->type,
             $this->xmlCollection,
@@ -131,6 +133,7 @@ class PropertyMetadata extends BasePropertyMetadata
             $this->sinceVersion,
             $this->untilVersion,
             $this->groups,
+            $this->excludeForGroups,
             $this->serializedName,
             $this->type,
             $this->xmlCollection,
