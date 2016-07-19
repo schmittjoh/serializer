fbf5a472e1d112f8dc7d5704bc3cbdf2e7949f86
Maximilian Ruta <mr@deltachaos.de>
2016-07-19 18:45:25 +0200

    handler_callbacks in YAML mapping handled correctly

--- src/JMS/Serializer/Metadata/Driver/YamlDriver.php
+++ src/JMS/Serializer/Metadata/Driver/YamlDriver.php
@@ -205,8 +205,11 @@ class YamlDriver extends AbstractFileDriver
         if (isset($config['handler_callbacks'])) {
             foreach ($config['handler_callbacks'] as $direction => $formats) {
                 foreach ($formats as $format => $methodName) {
-                    $direction = GraphNavigator::parseDirection($direction);
-                    $metadata->addHandlerCallback($direction, $format, $methodName);
+                    $metadata->addHandlerCallback(
+                        GraphNavigator::parseDirection($direction),
+                        $format,
+                        $methodName
+                    );
                 }
             }
         }
