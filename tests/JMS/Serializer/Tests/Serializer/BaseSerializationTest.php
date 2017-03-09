7c0ed4e3c34d295fa205287cfed4d25305d54cd3
Asmir Mustafic <goetas@gmail.com>
2017-03-09 08:15:15 +0100

    reverted variable renaming

--- tests/JMS/Serializer/Tests/Serializer/BaseSerializationTest.php
+++ tests/JMS/Serializer/Tests/Serializer/BaseSerializationTest.php
@@ -596,13 +596,14 @@ abstract class BaseSerializationTest extends \PHPUnit_Framework_TestCase
 
     public function testDateInterval()
     {
-        $value = new \DateInterval('PT45M');
-        $this->assertEquals($this->getContent('date_interval'), $this->serializer->serialize($value, $this->getFormat()));
+        $duration = new \DateInterval('PT45M');
+
+        $this->assertEquals($this->getContent('date_interval'), $this->serializer->serialize($duration, $this->getFormat()));
 
         if ($this->hasDeserializer()) {
             $deserialized = $this->deserialize($this->getContent('date_interval'), \DateInterval::class);
-            $this->assertEquals($value, $deserialized);
-            $this->assertEquals($value->i, $deserialized->i);
+            $this->assertEquals($duration, $deserialized);
+            $this->assertEquals($duration->i, $deserialized->i);
         }
     }
 
