b9f9329bc181210bf7ad62d8ed27a117c16ae67e
Asmir Mustafic <goetas@gmail.com>
2017-04-21 18:20:25 +0200

    Add for an event the possibility to stop propagation and pass more info to the param listener
    (inspired by the symfony 3.x event dispatcher implementation)

--- src/JMS/Serializer/EventDispatcher/Event.php
+++ src/JMS/Serializer/EventDispatcher/Event.php
@@ -22,6 +22,11 @@ use JMS\Serializer\Context;
 
 class Event
 {
+    /**
+     * @var bool Whether no further event listeners should be triggered
+     */
+    private $propagationStopped = false;
+
     protected $type;
     private $context;
 
@@ -44,5 +49,28 @@ class Event
     public function getType()
     {
         return $this->type;
+    }
+
+    /**
+     * Returns whether further event listeners should be triggered.
+     *
+     * @see Event::stopPropagation()
+     *
+     * @return bool Whether propagation was already stopped for this event
+     */
+    public function isPropagationStopped()
+    {
+        return $this->propagationStopped;
+    }
+    /**
+     * Stops the propagation of the event to further event listeners.
+     *
+     * If multiple event listeners are connected to the same event, no
+     * further event listener will be triggered once any trigger calls
+     * stopPropagation().
+     */
+    public function stopPropagation()
+    {
+        $this->propagationStopped = true;
     }
 }
