aed29cfeaec8fd6ab8ca484785a1adcf2e2efe6c
Asmir Mustafic <goetas@gmail.com>
2017-05-24 11:55:33 +0200

    changed unit test, using more mock strategies

--- tests/Exclusion/ExpressionLanguageExclusionStrategyTest.php
+++ tests/Exclusion/ExpressionLanguageExclusionStrategyTest.php
@@ -1,67 +1,75 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Aleksander Lanes
- * Date: 23/05/2017
- * Time: 19:58
+
+/*
+ * Copyright 2016 Johannes M. Schmitt <schmittjoh@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 namespace JMS\Serializer\Tests\Exclusion;
 
-use JMS\Serializer\DeserializationContext;
 use JMS\Serializer\Exclusion\ExpressionLanguageExclusionStrategy;
 use JMS\Serializer\Expression\ExpressionEvaluator;
 use JMS\Serializer\Metadata\StaticPropertyMetadata;
-use Symfony\Component\ExpressionLanguage\ExpressionLanguage;
+use JMS\Serializer\SerializationContext;
 
+/**
+ * @author Asmir Mustafic <goetas@gmail.com>
+ */
 class ExpressionLanguageExclusionStrategyTest extends \PHPUnit_Framework_TestCase
 {
-    /**
-     * @dataProvider getExpressions
-     */
-    public function testExpressionLanguageExclusionWorks($excludeIf, $expectedResult)
+    private $visitedObject;
+    private $context;
+    private $expressionEvaluator;
+    private $exclusionStrategy;
+
+    public function setUp()
+    {
+        $this->visitedObject = new \stdClass();
+
+        $this->context = $this->getMockBuilder(SerializationContext::class)->getMock();
+        $this->context->method('getObject')->willReturn($this->visitedObject);
+
+        $this->expressionEvaluator = $this->getMockBuilder(ExpressionEvaluator::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->exclusionStrategy = new ExpressionLanguageExclusionStrategy($this->expressionEvaluator);
+    }
+
+    public function testExpressionLanguageExclusionWorks()
     {
         $metadata = new StaticPropertyMetadata('stdClass', 'prop', 'propVal');
-        $metadata->excludeIf = $excludeIf;
+        $metadata->excludeIf = 'foo';
 
-        $context = DeserializationContext::create();
+        $this->expressionEvaluator->expects($this->once())
+            ->method('evaluate')
+            ->with('foo', array(
+                'context' => $this->context,
+                'property_metadata' => $metadata,
+                'object' => $this->visitedObject,
+            ))
+            ->willReturn(true);
 
-        $strat = $this->getExpressionLanguageExclusionStrategy();
-        $this->assertSame($expectedResult, $strat->shouldSkipProperty($metadata, $context));
+        $this->assertSame(true, $this->exclusionStrategy->shouldSkipProperty($metadata, $this->context));
     }
 
-    public function getExpressionLanguageExclusionStrategy()
+    public function testExpressionLanguageSkipsWhenNoExpression()
     {
-        return new ExpressionLanguageExclusionStrategy(new ExpressionEvaluator(new ExpressionLanguage()));
-    }
+        $metadata = new StaticPropertyMetadata('stdClass', 'prop', 'propVal');
 
-    public function getExpressions()
-    {
-        return [
-            'Test should be excluded'     => ['true', true],
-            'Test should not be excluded' => ['false', false],
-        ];
-    }
+        $this->expressionEvaluator->expects($this->never())->method('evaluate');
 
-    /**
-     * @dataProvider getVariables
-     */
-    public function testContextAndMetadataAreAvailableAsExpressionLanguageVariables($variable)
-    {
-        $property_metadata = new StaticPropertyMetadata('stdClass', 'prop', 'propVal');
-        $property_metadata->excludeIf = $variable;
-
-        $context = DeserializationContext::create();
-
-        $strat = $this->getExpressionLanguageExclusionStrategy();
-        $this->assertSame($$variable, $strat->shouldSkipProperty($property_metadata, $context));
-    }
-
-    public function getVariables()
-    {
-        return [
-            'Test context is available'           => ['context'],
-            'Test property_metadata is available' => ['property_metadata'],
-        ];
+        $this->assertSame(false, $this->exclusionStrategy->shouldSkipProperty($metadata, $this->context));
     }
 }
