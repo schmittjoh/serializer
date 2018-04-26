5f3b4ba404a976a7700987582f6b76e7bdeb8451
Asmir Mustafic <goetas@gmail.com>
2018-04-26 17:41:58 +0200

    type casting tests

--- tests/Serializer/JsonSerializationTest.php
+++ tests/Serializer/JsonSerializationTest.php
@@ -116,6 +116,7 @@ class JsonSerializationTest extends BaseSerializationTest
             $outputs['object_with_object_property'] = '{"foo": "bar", "author": {"full_name": "baz"}}';
             $outputs['author_expression'] = '{"my_first_name":"Ruud","last_name":"Kamphuis","id":123}';
             $outputs['maxdepth_skippabe_object'] = '{"a":{"xxx":"yyy"}}';
+            $outputs['type_casting'] = '{"as_string":"8"}';
         }
 
         if (!isset($outputs[$key])) {
