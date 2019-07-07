0efd90ae5091231a387c29fb0249e9d244251ae4
Serghei Niculaev <spam312sn@gmail.com>
2019-07-08 00:37:49 +0300

    #1073, Fix exclusion policy bug

--- tests/Metadata/Driver/AnnotationDriverTest.php
+++ tests/Metadata/Driver/AnnotationDriverTest.php
@@ -7,9 +7,19 @@ namespace JMS\Serializer\Tests\Metadata\Driver;
 use Doctrine\Common\Annotations\AnnotationReader;
 use JMS\Serializer\Metadata\Driver\AnnotationDriver;
 use JMS\Serializer\Naming\IdenticalPropertyNamingStrategy;
+use JMS\Serializer\Tests\Fixtures\AllExcludedObject;
 
 class AnnotationDriverTest extends BaseDriverTest
 {
+    public function testAllExcluded()
+    {
+        $a = new AllExcludedObject();
+        $m = $this->getDriver()->loadMetadataForClass(new \ReflectionClass($a));
+
+        self::assertArrayNotHasKey('foo', $m->propertyMetadata);
+        self::assertArrayHasKey('bar', $m->propertyMetadata);
+    }
+
     protected function getDriver()
     {
         return new AnnotationDriver(new AnnotationReader(), new IdenticalPropertyNamingStrategy(), null, $this->getExpressionEvaluator());
