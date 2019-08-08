53eb7b70e89ebe611f6f7d021177ce6c65234886
Asmir Mustafic <goetas@gmail.com>
2019-08-08 09:07:48 +0200

    do not fail if xdebug is missing

--- .travis/install_test.sh
+++ .travis/install_test.sh
@@ -3,6 +3,6 @@
 set -ex
 
 if [[ $TRAVIS_PHP_VERSION  = '7.2' ]]; then PHPUNIT_FLAGS="--coverage-clover clover"; else PHPUNIT_FLAGS=""; fi
-if [[ $TRAVIS_PHP_VERSION != '7.2' ]]; then phpenv config-rm xdebug.ini; fi
+if [[ $TRAVIS_PHP_VERSION != '7.2' ]]; then phpenv config-rm xdebug.ini || true; fi
 composer self-update
 composer update $COMPOSER_FLAGS -n
