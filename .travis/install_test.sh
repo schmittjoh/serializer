fe1afc16967a27e5dd030f79eae524cf5ac2a6bc
Asmir Mustafic <goetas@gmail.com>
2019-08-15 13:29:13 +0200

    use latest symfony cache

--- .travis/install_test.sh
+++ .travis/install_test.sh
@@ -2,7 +2,10 @@
 
 set -ex
 
+composer self-update
+
 if [[ $TRAVIS_PHP_VERSION  = '7.2' ]]; then PHPUNIT_FLAGS="--coverage-clover clover"; else PHPUNIT_FLAGS=""; fi
 if [[ $TRAVIS_PHP_VERSION != '7.2' ]]; then phpenv config-rm xdebug.ini || true; fi
-composer self-update
+if [[ $TRAVIS_PHP_VERSION == '7.4'* ]]; then composer require --dev --no-update "symfony/cache:^4.4@dev"; fi
+
 composer update $COMPOSER_FLAGS -n
