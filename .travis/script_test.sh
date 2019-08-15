ddfe39faf4b94d508dfe9b039d8750be2ef77746
Asmir Mustafic <goetas@gmail.com>
2019-08-15 13:34:48 +0200

    skip phpcs with php7

--- .travis/script_test.sh
+++ .travis/script_test.sh
@@ -6,4 +6,6 @@ vendor/bin/phpunit $PHPUNIT_FLAGS
 phpenv config-rm xdebug.ini || true
 php tests/benchmark.php json 3
 php tests/benchmark.php xml 3
-vendor/bin/phpcs
+
+if [[ $TRAVIS_PHP_VERSION != '7.4'* ]]; then vendor/bin/phpcs; fi
+
