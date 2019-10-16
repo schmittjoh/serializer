dc2cd5145013d61a6e4358e64b0d8153aa82cbb9
Asmir Mustafic <goetas@gmail.com>
2019-10-16 16:14:53 +0200

    create PHPUNIT_FLAGS in parent shell

--- .travis/install_test.sh
+++ .travis/install_test.sh
@@ -4,7 +4,6 @@ set -ex
 
 composer self-update
 
-if [[ $TRAVIS_PHP_VERSION  = '7.2' ]]; then export PHPUNIT_FLAGS="--coverage-clover clover"; else PHPUNIT_FLAGS=""; fi
 if [[ $TRAVIS_PHP_VERSION != '7.2' ]]; then phpenv config-rm xdebug.ini || true; fi
 if [[ $TRAVIS_PHP_VERSION == '7.4'* ]]; then composer require --dev --no-update "symfony/cache:^4.4@dev"; fi
 
