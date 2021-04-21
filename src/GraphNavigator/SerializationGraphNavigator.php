662022321dfa6fc6aa40bea1ed1bac4a920b044d
David Buchmann <david.buchmann@liip.ch>
2021-04-21 13:59:02 +0200

    Update SerializationGraphNavigator.php

--- src/GraphNavigator/SerializationGraphNavigator.php
+++ src/GraphNavigator/SerializationGraphNavigator.php
@@ -236,9 +236,9 @@ final class SerializationGraphNavigator extends GraphNavigator
                 }
 
                 if (!is_object($data)) {
-                    throw new InvalidArgumentException('Value at ' . $this->context->getPath() . ' is expected to be an object of class ' . $type['name'] . ' but is of type ' . csgettype($data));
+                    throw new InvalidArgumentException('Value at ' . $this->context->getPath() . ' is expected to be an object of class ' . $type['name'] . ' but is of type ' . gettype($data));
                 }
-                
+
                 $this->context->pushClassMetadata($metadata);
 
                 foreach ($metadata->preSerializeMethods as $method) {
