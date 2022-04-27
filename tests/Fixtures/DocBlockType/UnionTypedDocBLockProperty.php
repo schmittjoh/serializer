fefba5866465d15fd06d71480885067b9e136971
Dariusz Gafka <dariuszgafka@gmail.com>
2022-04-27 18:18:48 +0200

    Fix tests

--- tests/Fixtures/DocBlockType/UnionTypedDocBLockProperty.php
+++ tests/Fixtures/DocBlockType/UnionTypedDocBLockProperty.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace JMS\Serializer\Tests\Fixtures\DocBlockType;
 
-use JMS\Serializer\Tests\Fixtures\DocBlockType\Collection\Details\ProductDescription;
-
 class UnionTypedDocBLockProperty
 {
     /**
