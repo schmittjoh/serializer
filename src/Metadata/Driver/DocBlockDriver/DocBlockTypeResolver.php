5dc1fda8b77746330f0e0b5e4550a0f11b0bf0c9
Dariusz Gafka <dariuszgafka@gmail.com>
2023-01-02 17:58:54 +0100

    readonly when docblock defined

--- src/Metadata/Driver/DocBlockDriver/DocBlockTypeResolver.php
+++ src/Metadata/Driver/DocBlockDriver/DocBlockTypeResolver.php
@@ -228,13 +228,15 @@ final class DocBlockTypeResolver
             }
         }
 
-        preg_match_all(self::PHPSTAN_ARRAY_SHAPE, $declaringClass->getDocComment(), $foundPhpstanArrayShape);
-        $phpstanArrayShapes = array_map(static function (string $phpstanArrayType) {
-            return $phpstanArrayType;
-        }, $foundPhpstanArrayShape[1]);
+        if ($declaringClass->getDocComment()) {
+            preg_match_all(self::PHPSTAN_ARRAY_SHAPE, $declaringClass->getDocComment(), $foundPhpstanArrayShape);
+            $phpstanArrayShapes = array_map(static function (string $phpstanArrayType) {
+                return $phpstanArrayType;
+            }, $foundPhpstanArrayShape[1]);
 
-        if (in_array($typeHint, $phpstanArrayShapes)) {
-            return 'array';
+            if (in_array($typeHint, $phpstanArrayShapes)) {
+                return 'array';
+            }
         }
 
         throw new \InvalidArgumentException(sprintf("Can't use incorrect type %s for collection in %s:%s", $typeHint, $declaringClass->getName(), $reflectionProperty->getName()));
