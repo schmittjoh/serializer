b69fe07004ae2dc5602ce3007f11377c2479eccf
Marcin Czarnecki <scyzoryck@users.noreply.github.com>
2023-12-09 12:50:37 +0100

    Update phpstan/ignore-by-php-version.neon.php

--- phpstan/ignore-by-php-version.neon.php
+++ phpstan/ignore-by-php-version.neon.php
@@ -10,7 +10,7 @@ if (PHP_VERSION_ID < 80100) {
     $includes[] = __DIR__ . '/no-enum.neon';
 }
 if (PHP_VERSION_ID >= 80000) {
-    $includes[] = __DIR__ . '/ignore-missing-attributes.neon';
+    $includes[] = __DIR__ . '/ignore-missing-attribute.neon';
 }
 if (PHP_VERSION_ID >= 80100 && PHP_VERSION_ID < 80200) {
     $includes[] = __DIR__ . '/php-81.neon';
