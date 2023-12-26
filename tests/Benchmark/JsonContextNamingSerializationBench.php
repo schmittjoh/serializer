8247a39a526a9e892430752fe6248d6a0da4eca3
Dariusz Gafka <dariuszgafka@gmail.com>
2023-12-26 15:09:28 +0100

    phpstan

--- tests/Benchmark/JsonContextNamingSerializationBench.php
+++ tests/Benchmark/JsonContextNamingSerializationBench.php
@@ -6,11 +6,13 @@ namespace JMS\Serializer\Tests\Benchmark;
 
 use JMS\Serializer\Naming\IdenticalPropertyNamingStrategy;
 use JMS\Serializer\SerializationContext;
+use JMS\Serializer\Tests\Benchmark\Performance\JsonSerializationBench;
 
 class JsonContextNamingSerializationBench extends JsonSerializationBench
 {
     protected function createContext(): SerializationContext
     {
+        /** @phpstan-ignore-next-line */
         return parent::createContext()->setPropertyNamingStrategy(new IdenticalPropertyNamingStrategy());
     }
 }
