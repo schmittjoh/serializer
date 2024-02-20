3a3518a5b568cd2919ed95b3d749ba823ad52140
Jakub Pastuszek <jakub.pastuszek@invia.cz>
2024-02-20 16:31:44 +0100

    Use instanceof instead of is_a()

--- src/Handler/EnumHandler.php
+++ src/Handler/EnumHandler.php
@@ -44,7 +44,7 @@ final class EnumHandler implements SubscribingHandlerInterface
         array $type,
         SerializationContext $context
     ) {
-        if ((isset($type['params'][1]) && 'value' === $type['params'][1]) || (!isset($type['params'][1]) && is_a($enum, \BackedEnum::class, true))) {
+        if ((isset($type['params'][1]) && 'value' === $type['params'][1]) || (!isset($type['params'][1]) && $enum instanceof \BackedEnum)) {
             if (!$enum instanceof \BackedEnum) {
                 throw new InvalidMetadataException(sprintf('The type "%s" is not a backed enum, thus you can not use "value" as serialization mode for its value.', get_class($enum)));
             }
