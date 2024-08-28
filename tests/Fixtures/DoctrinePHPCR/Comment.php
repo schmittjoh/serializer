51f6a49fd601e7e56976c93383acb3cb63c940f6
Marcin Czarnecki <marcin.czarnecki@westwing.pl>
2024-08-28 19:38:04 +0200

    Doctrine ODM: fix tests for 2.0

--- tests/Fixtures/DoctrinePHPCR/Comment.php
+++ tests/Fixtures/DoctrinePHPCR/Comment.php
@@ -6,18 +6,25 @@ namespace JMS\Serializer\Tests\Fixtures\DoctrinePHPCR;
 
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ODM\PHPCR\Mapping\Annotations as PHPCRODM;
+use Doctrine\ODM\PHPCR\Mapping\Attributes\Document;
+use Doctrine\ODM\PHPCR\Mapping\Attributes\Field;
+use Doctrine\ODM\PHPCR\Mapping\Attributes\Id;
+use Doctrine\ODM\PHPCR\Mapping\Attributes\ReferenceOne;
 
 /** @PHPCRODM\Document */
+#[Document]
 class Comment
 {
     /**
      * @PHPCRODM\Id()
      */
+    #[Id]
     protected $id;
 
     /**
      * @PHPCRODM\ReferenceOne(targetDocument="Author")
      */
+    #[ReferenceOne(targetDocument:'Author')]
     private $author;
 
     /** @PHPCRODM\ReferenceOne(targetDocument="BlogPost") */
@@ -26,6 +33,7 @@ class Comment
     /**
      * @PHPCRODM\Field(type="string")
      */
+    #[Field(type: 'string')]
     private $text;
 
     public function __construct(Author $author, $text)
