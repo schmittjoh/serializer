8890b1301e6fcb5211bc58ba688ce5c40d8f6c2a
Niko <niko@fiteco.fi>
2026-03-12 01:13:18 +0200

    Cache DateTimeZone instances in DateHandler
    Avoid creating new DateTimeZone objects on every deserialization call for the same timezone string. Instance-level caching.

--- src/Handler/DateHandler.php
+++ src/Handler/DateHandler.php
@@ -38,6 +38,11 @@ final class DateHandler implements SubscribingHandlerInterface
     private $xmlCData;
 
     /**
+     * @var array<string, \DateTimeZone>
+     */
+    private $timezoneCache = [];
+
+    /**
      * {@inheritdoc}
      */
     public static function getSubscribingMethods()
@@ -250,7 +255,7 @@ final class DateHandler implements SubscribingHandlerInterface
      */
     private function parseDateTime($data, array $type, bool $immutable = false): \DateTimeInterface
     {
-        $timezone = !empty($type['params'][1]) ? new \DateTimeZone($type['params'][1]) : $this->defaultTimezone;
+        $timezone = !empty($type['params'][1]) ? $this->resolveTimezone($type['params'][1]) : $this->defaultTimezone;
         $formats = $this->getDeserializationFormats($type);
 
         $formatTried = [];
@@ -296,6 +301,15 @@ final class DateHandler implements SubscribingHandlerInterface
         }
 
         return $dateInterval;
+    }
+
+    private function resolveTimezone(string $timezone): \DateTimeZone
+    {
+        if (!isset($this->timezoneCache[$timezone])) {
+            $this->timezoneCache[$timezone] = new \DateTimeZone($timezone);
+        }
+
+        return $this->timezoneCache[$timezone];
     }
 
     /**
